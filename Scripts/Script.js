f52e7033198d49b3370fdf04cb371a6425cb6cfa
KulaniKhoza <kulanikhoza16@gmail.com>
2025-05-22 01:51:10 +0200

    backtotopbutton

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -33,6 +33,27 @@ window.addEventListener("resize", () => {
     closeBtn.style.display = "none";
   }
 });
+window.onload = function () {
+  const backToTopBtn = document.getElementById("backToTopBtn");
+
+  window.addEventListener("scroll", function () {
+    if (
+      document.body.scrollTop > 300 ||
+      document.documentElement.scrollTop > 300
+    ) {
+      backToTopBtn.classList.add("show");
+    } else {
+      backToTopBtn.classList.remove("show");
+    }
+  });
+
+  backToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+};
 
 // Smooth scroll to contact section
 function scrollToContact() {
